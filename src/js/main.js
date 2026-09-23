d70f642e4444315f799c1970b0acc688c84fd249
sgoodmansen <goo25023@byui.edu>
2026-09-23 10:49:59 -0600

    update

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,28 @@
 import { getParkData } from "./parkService.mjs";
 
 const parkData = getParkData();
+
+//update the disclaimer link
+const disclaimer = document.querySelector(".disclaimer > a");
+disclaimer.href = parkData.url;
+disclaimer.innerHTML = parkData.fullName;
+
+//update the browser tab title
+document.title = parkData.fullName;
+
+//update the hero image
+const heroImage = document.querySelector(".hero-banner > img");
+heroImage.src = parkData.images[0].url;
+heroImage.alt = parkData.images[0].altText;
+
+//update the park information displayed over the hero image
+const heroContent = document.querySelector(".hero-banner__content");
+heroContent.innerHTML = parkInfoTemplate(parkData);
+
+function parkInfoTemplate(info) {
+  return `<a href="/" class="hero-banner__title">${info.name}</a>
+  <p class="hero-banner__subtitle">
+    <span>${info.designation}</span>
+    <span>${info.states}</span>
+  </p>`;
+}
